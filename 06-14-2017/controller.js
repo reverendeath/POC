186e4f3445b07f62567437cea296e9b3ba0b9d23
meesalakr <kotislr75@gmail.com>
2017-07-07 14:57:38 +0530

    Update controller.js

--- 06-14-2017/controller.js
+++ 06-14-2017/controller.js
@@ -1,7 +1,8 @@
 var app=angular.module("myApp",["ui.router"]);
 
 
-app.config(function($stateProvider){
+app.config(function($stateProvider,$locationProvider){
+	$locationProvider.html5Mode(true);
 $stateProvider
 .state('home',{
    url: '/home',
@@ -9,7 +10,8 @@ $stateProvider
 })
 .state('form',{
    url: '/form',
-   templateUrl: 'form.html'
+   templateUrl: 'form.html',
+   controller: 'formController'
 })
 .state('form.msg',{
    templateUrl: 'msg.html'
@@ -29,5 +31,32 @@ $stateProvider
 app.controller("myController",function($scope,$location,$rootScope){
 
  
+});
+
+
+app.controller("formController",function($scope){
+$scope.servers=[];
+$scope.db=[];
+$scope.$watch('servers', function(newValue,oldValue){
+	console.log($scope.servers);
+	$scope.count=$scope.servers.length;
+});
+$scope.$watch('db', function(newValue,oldValue){
+	console.log($scope.db);
+	$scope.dbcount=$scope.db.length;
+});
+$scope.environment=['Development','Test','Production','Stage','DR'];
+$scope.options=["Yes","No","NA"];
+
+    $('.form_datetime').datetimepicker({
+        //language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		forceParse: 0,
+        showMeridian: 1
+    });
  
 });
